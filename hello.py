05472d2be26f225b354126c1ea054ff89f26dcae
guowang.sun <guowang.sun@qunar.com>
2015-02-14 17:16:35 +0800

    add utf-8 encoding

--- hello.py
+++ hello.py
@@ -1,3 +1,6 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import time
 name = raw_input('Please enter your name : ')
 print 'Hello,', name
+print time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
